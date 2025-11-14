564b33cd59c7b82e5b8071fb33c580b6eda1ade0
LilianFarina <lilian.loyola@outlook.com>
2025-11-14 10:08:57 -0300

    Update sortable_page.py

--- pages/sortable_page.py
+++ pages/sortable_page.py
@@ -1,1 +1,19 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
+from pages.base_page import BasePage
+
+class SortablePage(BasePage):
+
+    SORTABLE = (By.XPATH, "//span[text()='Sortable']")
+    ITEMS = (By.CSS_SELECTOR, "#demo-tabpane-list div")
+
+    def open_sortable(self):
+        self.click(self.SORTABLE)
+
+    def sort_items(self):
+        items = self.driver.find_elements(*self.ITEMS)
+        action = ActionChains(self.driver)
+
+        for i in range(len(items) - 1):
+            action.drag_and_drop(items[i+1], items[i]).perform()
 
