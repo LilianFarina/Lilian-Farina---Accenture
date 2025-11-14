31d128853c00ef284c572476ede533dc8a1595b8
LilianFarina <lilian.loyola@outlook.com>
2025-11-14 10:09:53 -0300

    Update test_webtables.py

--- tests/test_webtables.py
+++ tests/test_webtables.py
@@ -1,1 +1,22 @@
+from utils.driver_factory import create_driver
+from pages.home_page import HomePage
+from pages.webtables_page import WebTablesPage
+from faker import Faker
+
+def test_webtables():
+    driver = create_driver()
+    faker = Faker()
+
+    driver.get("https://demoqa.com/")
+    home = HomePage(driver)
+    page = WebTablesPage(driver)
+
+    home.click_elements()
+    page.open_webtables()
+
+    page.create_record(faker)
+    page.edit_first_record()
+    page.delete_first_record()
+
+    driver.quit()
 
