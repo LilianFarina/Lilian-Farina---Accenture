5fa2debad209e17281a8d522ac8e2e2e5345253b
LilianFarina <lilian.loyola@outlook.com>
2025-11-14 10:10:08 -0300

    Update test_progressbar.py

--- tests/test_progressbar.py
+++ tests/test_progressbar.py
@@ -1,1 +1,20 @@
+from utils.driver_factory import create_driver
+from pages.home_page import HomePage
+from pages.progressbar_page import ProgressBarPage
+
+def test_progressbar():
+    driver = create_driver()
+    driver.get("https://demoqa.com/")
+
+    home = HomePage(driver)
+    page = ProgressBarPage(driver)
+
+    home.click_widgets()
+    page.open_progressbar()
+
+    page.start()
+    page.wait_until_25()
+    page.reset()
+
+    driver.quit()
 
