6b873b7039f8d9d0925a504cfcfb3d35df76a49c
LilianFarina <lilian.loyola@outlook.com>
2025-11-14 10:07:29 -0300

    Update home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,1 +1,26 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+
+class HomePage(BasePage):
+
+    FORMS = (By.XPATH, "//h5[text()='Forms']")
+    ALERTS = (By.XPATH, "//h5[text()='Alerts, Frame & Windows']")
+    ELEMENTS = (By.XPATH, "//h5[text()='Elements']")
+    WIDGETS = (By.XPATH, "//h5[text()='Widgets']")
+    INTERACTIONS = (By.XPATH, "//h5[text()='Interactions']")
+
+    def click_forms(self):
+        self.click(self.FORMS)
+
+    def click_alerts(self):
+        self.click(self.ALERTS)
+
+    def click_elements(self):
+        self.click(self.ELEMENTS)
+
+    def click_widgets(self):
+        self.click(self.WIDGETS)
+
+    def click_interactions(self):
+        self.click(self.INTERACTIONS)
 
