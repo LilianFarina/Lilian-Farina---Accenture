e1e0b808c0332a4d6ff9088595d27cb75cdddced
LilianFarina <lilian.loyola@outlook.com>
2025-11-14 10:09:36 -0300

    Update test_browser_windows.py

--- tests/test_browser_windows.py
+++ tests/test_browser_windows.py
@@ -1,1 +1,20 @@
+from utils.driver_factory import create_driver
+from pages.home_page import HomePage
+from pages.browser_windows_page import BrowserWindowsPage
+
+def test_browser_windows():
+    driver = create_driver()
+    driver.get("https://demoqa.com/")
+
+    home = HomePage(driver)
+    page = BrowserWindowsPage(driver)
+
+    home.click_alerts()
+    page.open_menu()
+    page.open_new_window()
+
+    assert page.validate_new_window()
+    page.close_new_window()
+
+    driver.quit()
 
