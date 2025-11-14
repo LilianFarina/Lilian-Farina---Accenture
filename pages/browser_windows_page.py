0ba0a9daa712ba73858f76e23ab52a734d765682
LilianFarina <lilian.loyola@outlook.com>
2025-11-14 10:08:03 -0300

    Update browser_windows_page.py

--- pages/browser_windows_page.py
+++ pages/browser_windows_page.py
@@ -1,1 +1,26 @@
+from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
+
+class BrowserWindowsPage(BasePage):
+
+    BROWSER_WINDOWS = (By.XPATH, "//span[text()='Browser Windows']")
+    NEW_WINDOW = (By.ID, "windowButton")
+    SAMPLE_TEXT = (By.ID, "sampleHeading")
+
+    def open_menu(self):
+        self.click(self.BROWSER_WINDOWS)
+
+    def open_new_window(self):
+        self.click(self.NEW_WINDOW)
+
+    def validate_new_window(self):
+        handles = self.driver.window_handles
+        self.driver.switch_to.window(handles[1])
+        text = self.get_text(self.SAMPLE_TEXT)
+        return "This is a sample page" in text
+
+    def close_new_window(self):
+        self.driver.close()
+        handles = self.driver.window_handles
+        self.driver.switch_to.window(handles[0])
 
