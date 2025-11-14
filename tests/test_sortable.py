cf9f9a320535e5674012c412463e9f58669bc10a
LilianFarina <lilian.loyola@outlook.com>
2025-11-14 10:10:25 -0300

    Update test_sortable.py

--- tests/test_sortable.py
+++ tests/test_sortable.py
@@ -1,1 +1,17 @@
+from utils.driver_factory import create_driver
+from pages.home_page import HomePage
+from pages.sortable_page import SortablePage
+
+def test_sortable():
+    driver = create_driver()
+    driver.get("https://demoqa.com/")
+
+    home = HomePage(driver)
+    page = SortablePage(driver)
+
+    home.click_interactions()
+    page.open_sortable()
+    page.sort_items()
+
+    driver.quit()
 
