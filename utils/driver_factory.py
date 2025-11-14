4742382fe3390bf0ac4960a2b659aa618bc2254f
LilianFarina <lilian.loyola@outlook.com>
2025-11-14 10:06:47 -0300

    Update driver_factory.py

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,1 +1,8 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+def create_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+    return webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
